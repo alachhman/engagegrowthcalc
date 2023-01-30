8bb17d91083c9abb5342e27e048e466ea1208796
alachhman <33943685+alachhman@users.noreply.github.com>
2023-01-29 22:09:45 -0500

    added another stat output box

--- engagegrowthcalc/src/components/Calculator.js
+++ engagegrowthcalc/src/components/Calculator.js
@@ -13,36 +13,52 @@ import Tooltip from "./Tooltip";
 let Calculator = () => {
     const [Unit, setUnit] = React.useState(unitGrowth[0]);
     const [Class, setClass] = React.useState(classGrowth[0])
+    const [Class2, setClass2] = React.useState(classGrowth[0])
     const [StarSphere, setStarSphere] = React.useState(false)
 
     useEffect(x => {
         console.log({
             Unit: Unit,
             Class: Class,
+            Class2: Class2,
             StarSphere: StarSphere
         })
     })
 
     return (
-        <div className="main-content">
+        <div>
             <div className="main-options">
                 <Selection name="Unit" pool={unitGrowth} set={setUnit}/> |
                 <Selection name="Class" pool={classGrowth} set={setClass}/> |
+                <Selection name="Class" pool={classGrowth} set={setClass2}/> |
                 <CheckBox label="Starsphere" set={setStarSphere}/>
             </div>
-            <div className="main-results">
-                <strong>Growths:</strong>
-                <Divider color={"white"}/>
-                <div className="main-results-table">
-                    {Object.keys(Unit).filter(x => x !== "name").map(x => {
-                        return (
-                            <StatOut statName={x.toUpperCase()}
-                                     unitStat={Unit[x]}
-                                     classStat={(Unit.name === "Jean" ? Class[x] * 2: Class[x])}
-                                     starSphere={StarSphere}/>
-                        )
-                    })}
-                </div>
+            <div className="main-contents">
+                <StatGraph unit={Unit} class={Class} ss={StarSphere}/>
+                <StatGraph unit={Unit} class={Class2} ss={StarSphere}/>
+            </div>
+        </div>
+    )
+}
+
+let StatGraph = (props) => {
+    let Unit = props.unit;
+    let Class = props.class;
+    let StarSphere = props.ss;
+
+    return (
+        <div className="main-results">
+            <strong>Growths: {" " + Class.name}</strong>
+            <Divider color={"white"}/>
+            <div className="main-results-table">
+                {Object.keys(Unit).filter(x => x !== "name").map(x => {
+                    return (
+                        <StatOut statName={x.toUpperCase()}
+                                 unitStat={Unit[x]}
+                                 classStat={(Unit.name === "Jean" ? Class[x] * 2 : Class[x])}
+                                 starSphere={StarSphere}/>
+                    )
+                })}
             </div>
         </div>
     )
@@ -58,7 +74,8 @@ let StatOut = (props) => {
 
     return (
         <div className="statout">
-            <Tooltip direction={"right"} content={"Unit Growth (" + unitStat + ") + Class Growth (" + classStat + ") " + (starSphere ? "+ StarSphere (15)" : "")}>
+            <Tooltip direction={"right"}
+                     content={"Unit Growth (" + unitStat + ") + Class Growth (" + classStat + ") " + (starSphere ? "+ StarSphere (15)" : "")}>
                 <strong>{statname}:</strong> {total}
             </Tooltip>
             <div style={{maxWidth: total + "%"}}>
